474efc8b3f0c416bcdb7d7b48e0263aff3d14c32
Wanderson Vieira <77136542+vampireprin@users.noreply.github.com>
2021-01-17 10:48:30 -0300

    Add files via upload

---
+++ src/bruxinhomenu.js
@@ -0,0 +1,12 @@
+const bruxinhomenu = (prefix, pushname) => {
+    return `◪ *Comandos do bruxinho*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.bruxinhomenu = bruxinho
